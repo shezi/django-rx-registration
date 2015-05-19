c6ffae7c357033beac8625d1f4f20e90810885a4
Johannes Spielmann <j@spielmannsolutions.com>
2015-05-19 12:00:47 +0200

    i18n for templates and a bit of cleanup

--- rx-registration/forms.py
+++ rx-registration/forms.py
@@ -11,12 +11,14 @@ class LoginForm(forms.Form):
     username = forms.RegexField(
         regex=r'^[\w.@+-]+$',
         max_length=60,
-        label="Benutzername",
-        error_messages={'invalid': "Im Benutzernamen dürfen nur Zahlen, Buchstaben und die Zeichen ., +, -, _ und @ vorkommen."}
+        label=_("User name"),
+        error_messages={
+            'invalid': _("In the user name, only numbers, alphabetical characters and the characters  ., +, -, _ and @"
+                         " may appear.")},
     )
     password = forms.CharField(
         widget=forms.PasswordInput,
-        label="Passwort"
+        label=_("Password"),
     )
 
     def clean(self):
@@ -25,26 +27,22 @@ class LoginForm(forms.Form):
         if user is not None and user.is_active:
             self.user = user
             return self.cleaned_data
-        raise forms.ValidationError("Die Anmeldedaten können nicht gefunden werden. Bitte überprüfen Sie die Daten und versuchen Sie es erneut.")
+        raise forms.ValidationError(_("Your log in data could not be found. Please check your input and try again."))
 
 
 class RegistrationForm(forms.Form):
 
     username = forms.RegexField(regex=r'^[\w.@+-]+$',
                                 max_length=60,
-                                label="Benutzername",
-                                error_messages={'invalid': "Im Benutzernamen dürfen nur Zahlen, Buchstaben und die Zeichen ., +, -, _ und @ vorkommen."})
-    email = forms.EmailField(label="E-Mail")
+                                label=_("User name"),
+                                error_messages={
+                                    'invalid': _("In the user name, only numbers, alphabetical characters and the "
+                                                 "characters  ., +, -, _ and @ may appear.")})
+    email = forms.EmailField(label=_("Email"))
     password1 = forms.CharField(widget=forms.PasswordInput,
-                                label="Passwort")
+                                label=_("Password"))
     password2 = forms.CharField(widget=forms.PasswordInput,
-                                label="Passwort (erneut, zur Kontrolle)")
-
-    tos = forms.BooleanField(widget=forms.CheckboxInput,
-                             initial=False,
-                             label=mark_safe('Ich habe die <a href="/agb/">allgemeinen Geschäftsbedingungen</a> gelesen und stimme ihnen zu.'),
-                             error_messages={'required': "Sie müssen den AGB zustimmen, um sich registrieren zu können."})
-    tos.left = True
+                                label=_("Password (repeated for checking)"))
 
     def clean_username(self):
         """
@@ -53,7 +51,7 @@ class RegistrationForm(forms.Form):
         """
         existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
         if existing.exists():
-            raise forms.ValidationError("Dieser Benutzername ist bereits vergeben.")
+            raise forms.ValidationError(_("This username is already taken."))
         else:
             return self.cleaned_data['username']
 
@@ -61,7 +59,8 @@ class RegistrationForm(forms.Form):
         """Make sure the email isn't taken yet."""
         existing = User.objects.filter(email__iexact=self.cleaned_data['email'])
         if existing.exists():
-            raise forms.ValidationError("Diese Emailadresse wird bereits für einen Benutzer genutzt. Bitte geben Sie eine andere Emailadresse an!")
+            raise forms.ValidationError(_("This email address is already in use. Please enter a different email "
+                                          "address!"))
         else:
             return self.cleaned_data['email']
 
@@ -75,5 +74,6 @@ class RegistrationForm(forms.Form):
         """
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError("Die beiden Passwort-Felder stimmen nicht überein.")
+                raise forms.ValidationError(_("The two password fields are different. Please enter the same password "
+                                              "in both fields."))
         return self.cleaned_data
