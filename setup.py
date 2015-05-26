59ca6f5af3c90be13388282a6c5ddaad69bf1469
Johannes Spielmann <j@spielmannsolutions.com>
2015-05-26 19:57:44 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,44 @@
 
+import os
 from setuptools import find_packages, setup
 
-# based off django's own setup.py
+# based off django's own setup.py and on the one from django-extensions (which is great and you should install that!)
 
 version = __import__('rx_registration').__version__
+
+rx_dir = 'rx_registration'
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, package_data = [], {}
+
+for dirpath, dirnames, filenames in os.walk(rx_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames:
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
 
 setup(
     name='django-rx-registration',
@@ -14,7 +49,8 @@ setup(
     description=('A high-level Python Web framework that encourages '
                  'rapid development and clean, pragmatic design.'),
     license='MIT',
-    packages=find_packages(), #exclude=EXCLUDE_FROM_PACKAGES),
+    packages=packages,
+    package_data=package_data,
     include_package_data=True,
     zip_safe=False,
     classifiers=[
